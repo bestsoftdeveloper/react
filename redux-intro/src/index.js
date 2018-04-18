b124a1249166a03b18f60db8c1a53b867700d3d6
claudiu9379 <office@bestdeveloper.ro>
2018-04-18 13:38:15 +0300

    redux 4 - combine reducers

--- redux-intro/src/index.js
+++ redux-intro/src/index.js
@@ -8,21 +8,39 @@
 // registerServiceWorker();
 
 
-import { createStore } from "redux";
-const reducer = function(state, action){
+import { combineReducers, createStore } from "redux";
+
+const userReducer = (state={
+		user:{
+			name:"John",
+			age:0
+		}
+	}, action) =>{
+	if(action.type === "CHANGE_NAME"){
+		state = {...state, name: action.payload}
+	}
+	if(action.type === "CHANGE_AGE"){
+		state = {...state, age: action.payload}
+	}
+	return state;
+};
+
+const tweetsReducer = (state={
+		tweets:[]
+	}, action) =>{
 	if(action.type === "INC"){
 		return state+1;
 	}
 	return state;
 };
 
-const store = createStore(reducer,
-	{
-		user:{
-			name:"John",
-			tweets:[]
-		}
-	});
+const reducers = combineReducers(
+{
+	user:userReducer,
+	tweet:tweetsReducer
+});
+
+const store = createStore(reducers);
 
 store.subscribe(()=>{
 	console.log("store changed ", store.getState());
