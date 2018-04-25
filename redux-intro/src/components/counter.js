aebbe4cde9124155d7ff45b8b2196508605e74da
claudiu9379 <office@bestdeveloper.ro>
2018-04-25 14:16:58 +0300

    state local

--- redux-intro/src/components/counter.js
+++ redux-intro/src/components/counter.js
@@ -3,7 +3,10 @@ import React from 'react';
 class Counter extends React.Component {
     constructor(props) {
         super(props);
-
+        this.state = {
+            count: props.count
+        }
+        this.increase = this.increase.bind(this);
     }
 
     increment = () => {
@@ -17,14 +20,27 @@ class Counter extends React.Component {
         }
     }
 
+    increase(){
+        const newState = { ...this.state };
+        newState.count ++;
+
+        this.setState(newState);
+    }
+
     render() {
         return (
             <div>
-                <h2>Counter</h2>
+                {this.props.count % 2 === 0 &&
+                    <h2>Counter</h2>
+                }
                 <div>
                     <button onClick={this.decrement}>-</button>
                     <span>{this.props.count}</span>
                     <button onClick={this.increment}>+</button>
+
+                    <button onClick={this.increase}>increase</button>
+                    <span>{this.state.count}</span>
+
                 </div>
             </div>
         )
